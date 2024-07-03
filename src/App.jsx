85ce779dae3f08c6c066d61bc03650c3967d6b60
zumrudbaxshaliyeva <zumrudbaxsaliyeva@gmail.com>
2024-07-03 06:18:57 -0700

    random users

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+//https://randomuser.me/api/?results=5
+import Header from "./components/Header"
+import UserList from "./components/UserList"
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <UserList/>
+
+    </>
+  )
+}
+
+export default App
